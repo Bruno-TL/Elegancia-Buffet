cdf42bbf8cea118f2c11da131654a56ca5a504f1
Thiago Bruno <tbgt35@hotmail.com>
2023-01-04 22:38:11 -0300

    Estilizando a navegação

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,6 +2,11 @@ import {BrowserRouter,Route, Routes} from "react-router-dom";
 import Home from "../pages/Home";
 import Headers from "../components/Headers";
 import Contato from "../pages/Contato";
+import Infantil from "../pages/Infantil";
+import Casamento from "../pages/Casamento";
+import Debutante from "../pages/Debutante";
+import Outros from "../pages/Outros";
+
 
 export default function Routers() {
     return (
@@ -10,6 +15,10 @@ export default function Routers() {
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/contato" element={<Contato/>}/>
+                    <Route path="/infantil" element={<Infantil/>}/>
+                    <Route path="/casamento" element={<Casamento/>}/>
+                    <Route path="/debutante" element={<Debutante/>}/>
+                    <Route path="/outros" element={<Outros/>}/>
                 </Routes>
         </BrowserRouter>
     );
--- src/components/Headers/index.js
+++ src/components/Headers/index.js
@@ -2,29 +2,28 @@ import { Link } from 'react-router-dom';
 import {FaChevronDown} from 'react-icons/fa';
 import logo from '../../assets/logo_EleganciaBuffet.png';
 import './style.css';
+import { useState } from 'react';
 
 export default function Headers() {
-    const toggleMenu = () => {
-        document.body.classList.toggle("open");
-    }
+
     return (
         <header className='header'>
             <img src={logo} className="imgHeader" />
             <nav className='navbar nav__header'>
-                <button onClick={toggleMenu()} className='burger'></button>
+                
                 <div className='dropdowns'>
                     <div className='dropdown'>
                         <Link className='links' to='/'>Início</Link>
                         <Link className='links' to='/contato'>Contato</Link>
                         <button>
-                            Festas <FaChevronDown/>
+                            Eventos <FaChevronDown/>
                         </button>
                         <div className='dropdown-menu'>
                             <div className='div__links__menu'>
-                                <Link className='links__menu'>Festa Infantil</Link >
-                                <Link className='links__menu'>Casamento</Link >
-                                <Link className='links__menu'>Debutante</Link >
-                                <Link className='links__menu'>Outros</Link >
+                                <Link className='links__menu' to='/infantil'>Festa Infantil</Link >
+                                <Link className='links__menu' to='casamento'>Casamento</Link >
+                                <Link className='links__menu' to='debutante'>Debutante</Link >
+                                <Link className='links__menu' to='outros'>Outros</Link >
                             </div>
                         </div>
                     </div>
